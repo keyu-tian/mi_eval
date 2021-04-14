acc9a8a4bbbbc93761c4d7be046f72c571e7c0ab
tky_hm_xps13h <k_code@163.com>
2021-04-14 19:25:30 +0800

    init

---
+++ main.py
@@ -0,0 +1,140 @@
+from collections import OrderedDict, defaultdict
+
+import argparse
+import datetime
+from sklearn.feature_selection import mutual_info_classif
+import torch
+from torch.utils.data import DataLoader
+from torchvision.transforms import transforms
+import os
+from pandas import DataFrame
+# from calc_mi import mi
+
+from imagenet import SubImageNetDataset
+from resbackbone import load_r50backbone
+
+import linklink as link
+from tqdm import tqdm
+
+
+def link_init():
+    torch.cuda.set_device(int(os.environ['SLURM_LOCALID']))
+    link.initialize()
+    return link.get_rank(), link.get_world_size()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='MI test')
+    parser.add_argument('--num_classes', default=30, type=int)
+    parser.add_argument('--n_neighbors', default=3, type=int)
+    parser.add_argument('--batch_size', default=256, type=int)
+    args = parser.parse_args()
+    
+    rank, world_size = link_init()
+    
+    test_transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225), inplace=True),
+    ])
+
+    # ~ num_classes * 1280 images
+    test_data = SubImageNetDataset(num_classes=args.num_classes, root='/mnt/lustre/share/images', train=False, transform=test_transform, download=False)
+    test_len = len(test_data)
+    test_ld = DataLoader(test_data, batch_size=args.batch_size, shuffle=False, drop_last=False, num_workers=4, pin_memory=True)
+    
+    ckpts = [
+        '/mnt/lustre/share/tengjianing/modelzoo/MM/dy/DY_MTL_LV1_10_R50_convertBB.pth.tar',
+        '/mnt/lustre/share/tengjianing/modelzoo/MM/dy/DY_MTL_LV1_30_R50_convertBB.pth.tar',
+        '/mnt/lustre/share/tengjianing/modelzoo/CLIP/xueshuClip.pth.tar',
+    ]
+    ckpt_names = [
+        os.path.split(ckpt)[-1].replace('.tar', '').replace('.pth', '')
+        for ckpt in ckpts
+    ]
+    ckpt_idx = rank % len(ckpts)
+    ckpt = ckpts[ckpt_idx]
+    ckpt_name = ckpt_names[ckpt_idx]
+    inputs, features, labels = [], [], []
+    r50_bb, warning = load_r50backbone(ckpt)
+    if rank < len(ckpts):
+        print(f'[rk{rank}]: {warning or "nothing"}')
+    r50_bb = r50_bb.cuda()
+    r50_bb.eval()
+    
+    link.barrier()
+    if rank == 0:
+        bar = tqdm(test_ld)
+    else:
+        bar = test_ld
+    with torch.no_grad():
+        for x, y in bar:
+            bs = x.shape[0]
+            h = torch.from_numpy(r50_bb(x.cuda()).cpu().numpy())
+            y = torch.from_numpy(y.view(bs, 1).numpy().astype(int))
+            # inputs.append(x.numpy())
+            features.append(h)
+            labels.append(y)
+            if rank == 0:
+                bar.set_description_str('[extracting features]')
+                bar.set_postfix(OrderedDict({'imgs': test_len, 'cur_bs': bs, 'ckpt': ckpt_name}))
+    
+    # inputs = torch.cat(inputs, dim=0)
+    features = torch.cat(features, dim=0)
+    labels = torch.cat(labels, dim=0)
+    labels = labels.reshape(-1)
+    assert features.shape[0] == labels.shape[0] == test_len
+    
+    if rank == 0:
+        print(f'[rk{rank}]: features={features.dtype} {tuple(features.shape)},  labels.shape={labels.dtype} {tuple(labels.shape)}')
+
+    mi_values = mutual_info_classif(features, labels, n_neighbors=args.n_neighbors)
+    mi_mean = sum(mi_values) / len(mi_values)
+    
+    top_10_percent = max(1, round(len(mi_values) * 0.1))
+    mi_top_10_percent = sorted(mi_values, reverse=True)[:top_10_percent]
+    mi_top_10_percent = sum(mi_top_10_percent) / len(mi_top_10_percent)
+    
+    mi_max = max(mi_values)
+    
+    for i in range(len(ckpts)):
+        if ckpt_idx == i:
+            print(
+                f'[rk{rank}]: ckpt={ckpt}, mi info:\n    mean={mi_mean:.4f},  max={mi_max:.4f},  top={mi_top_10_percent:.4f}'
+            )
+        link.barrier()
+    
+    mi_means = torch.zeros(world_size)
+    mi_maxes = torch.zeros(world_size)
+    mi_topks = torch.zeros(world_size)
+    mi_means[rank] = mi_mean
+    mi_maxes[rank] = mi_max
+    mi_topks[rank] = mi_top_10_percent
+    link.allreduce(mi_means), link.allreduce(mi_maxes), link.allreduce(mi_topks)
+
+    if rank == 0:
+        all_mi_means, all_mi_maxes, all_mi_topks = defaultdict(list), defaultdict(list), defaultdict(list)
+        for rk in range(world_size):
+            idx = rk % len(ckpts)
+            all_mi_means[ckpts[idx]].append(mi_means[rk].item())
+            all_mi_maxes[ckpts[idx]].append(mi_maxes[rk].item())
+            all_mi_topks[ckpts[idx]].append(mi_topks[rk].item())
+        for k, v in all_mi_means.items(): v.insert(0, sum(v) / len(v))
+        for k, v in all_mi_maxes.items(): v.insert(0, sum(v) / len(v))
+        for k, v in all_mi_topks.items(): v.insert(0, sum(v) / len(v))
+        df = DataFrame({
+            'ckpt': ckpt_names,
+            'mean': [all_mi_means[ckpt][0] for ckpt in ckpts],
+            'max': [all_mi_maxes[ckpt][0] for ckpt in ckpts],
+            'top': [all_mi_topks[ckpt][0] for ckpt in ckpts],
+        })
+        print(df)
+        df.to_json(f'results_imn{args.num_classes}_neib{args.n_neighbors}_{datetime.datetime.now().strftime("[%m-%d %H:%M:%S]")}.json')
+    
+    link.barrier()
+    link.finalize()
+
+
+if __name__ == '__main__':
+    main()
---
+++ resbackbone.py
@@ -0,0 +1,191 @@
+from pprint import pformat
+
+import torch.nn as nn
+import math
+import torch
+
+# will be overwritten during runtime
+BN = torch.nn.BatchNorm2d
+Conv2d = torch.nn.Conv2d
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        
+        self.conv1 = Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = BN(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = BN(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = BN(planes)
+        self.conv2 = Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = BN(planes)
+        self.conv3 = Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = BN(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResBackbone(nn.Module):
+
+    def __init__(self, block, layers, deep_stem=False, norm_func=nn.BatchNorm2d, conv_func=nn.Conv2d):
+        super(ResBackbone, self).__init__()
+
+        global BN
+        global Conv2d
+
+        BN = norm_func
+        Conv2d = conv_func
+        
+        self.curr_inplanes = 64
+
+        self.deep_stem = deep_stem
+        if self.deep_stem:
+            self.conv1 = Conv2d(3, self.curr_inplanes // 2, kernel_size=3, stride=2, padding=1, bias=False)
+            self.bn1 = BN(self.curr_inplanes // 2)
+            self.conv2 = Conv2d(self.curr_inplanes // 2, self.curr_inplanes // 2, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn2 = BN(self.curr_inplanes // 2)
+            self.conv3 = Conv2d(self.curr_inplanes // 2, self.curr_inplanes, kernel_size=3, stride=1, padding=1, bias=False)
+            self.bn3 = BN(self.curr_inplanes)
+        else:
+            self.conv1 = Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            self.bn1 = BN(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        # maskrcnn
+        # for p in self.parameters():
+        #     p.requires_grad = False
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        # initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 1.0/float(n))
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.curr_inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                Conv2d(self.curr_inplanes, planes * block.expansion,
+                       kernel_size=1, stride=stride, bias=False),
+                BN(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.curr_inplanes, planes, stride, downsample))
+        self.curr_inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.curr_inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.relu(self.bn1(self.conv1(x)))
+        if self.deep_stem:
+            x = self.relu(self.bn2(self.conv2(x)))
+            x = self.relu(self.bn3(self.conv3(x)))
+        x = self.maxpool(x)
+
+        layer1 = self.layer1(x)
+        layer2 = self.layer2(layer1)
+        layer3 = self.layer3(layer2)
+        layer4 = self.layer4(layer3)
+
+        x = self.avgpool(layer4)
+        feature = torch.flatten(x, 1)    # pre_fc: (B, fea_dim)
+        return feature
+
+
+def load_r50backbone(ckpt: str, norm_func=nn.BatchNorm2d, conv_func=nn.Conv2d):
+    d = torch.load(ckpt, map_location='cpu')
+    if set(d.keys()) == {'state_dict'}:
+        d = d['state_dict']
+    conv1_shape = d['conv1.weight'].shape
+    deep_stem = conv1_shape[0] == 32
+    
+    r50_bb = ResBackbone(Bottleneck, [3, 4, 6, 3], deep_stem=deep_stem, norm_func=norm_func, conv_func=conv_func)
+    msg = r50_bb.load_state_dict(d, strict=False)
+    
+    unexpected_missing = [k for k in msg.missing_keys if not k.startswith('fc.')]
+    assert len(unexpected_missing) == 0, f'unexpected msg.missing_keys:\n{pformat(unexpected_missing)}'
+    
+    unexpected_extra = msg.unexpected_keys
+    if len(msg.unexpected_keys):
+        warning = f'[warning] msg.unexpected_keys:\n{pformat(unexpected_extra)}'
+    else:
+        warning = ''
+    return r50_bb, warning
+
