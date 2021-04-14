941b180fd68debf46277a5eaad5239e2be247bbe
tky_hm_xps13h <k_code@163.com>
2021-04-14 19:44:28 +0800

    spring.submit -r

--- exp/spring.sh
+++ exp/spring.sh
@@ -1,7 +1,7 @@
 REL_PATH=../
 
 PYTHONPATH=${PYTHONPATH}:${REL_PATH} GLOG_vmodule=MemcachedClient=-1 \
-spring.submit run --gpu -n8 \
+spring.submit run -r --gpu -n8 \
 --job-name MI_test \
 --cpus-per-task=6 \
 "python -u -m main --num_classes=${2:-"50"} --n_neighbors=5 --batch_size=256 "
