48fb4ad0fc5dcd8f250578f0756f21711472a053
tky_hm_xps13h <k_code@163.com>
2021-04-17 19:17:30 +0800

    hx calc

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main():
     }[cfg.dataset]
     
     test_data = clz(**kw)
-    test_ld = DataLoader(test_data, batch_size=cfg.batch_size, shuffle=True, drop_last=False, num_workers=4, pin_memory=True)
+    test_ld = DataLoader(test_data, batch_size=cfg.batch_size, shuffle=True, drop_last=False, num_workers=2, pin_memory=True)
     
     ckpts = cfg.checkpoints
     if len(ckpts) > world_size:
@@ -73,12 +73,13 @@ def main():
     ckpt = ckpts[ckpt_idx]
     ckpt_name = os.path.split(ckpt)[-1].replace('.tar', '').replace('.pth', '')
     r50_bb, warning = load_r50backbone(ckpt)
-    if rank < len(ckpts):
-        print(f'[rk{rank}]: {warning or "nothing"}')
+    for rk in range(len(ckpts)):
+        if rk == rank:
+            print(f'[rk{rank}, {ckpt_name}]: {warning or "nothing"}')
+        link.barrier()
     r50_bb = r50_bb.cuda()
     r50_bb.eval()
     
-    link.barrier()
     tot_bs, inputs, features, labels = 0, [], [], []
     with torch.no_grad():
         bar = tqdm(test_ld) if rank == 0 else test_ld
@@ -120,7 +121,7 @@ def main():
     hy_max, hx_max = max(hy_values), max(hx_values)
     hy_mean, hx_mean = np.mean(hy_values).item(), np.mean(hx_values).item()
     hy_top = np.mean(sorted(hy_values, reverse=True)[:max(1, round(len(hy_values) * 0.1))]).item()
-    hx_top = np.mean(sorted(hx_values, reverse=True)[:max(1, round(len(hx_values) * 0.1))]).item()
+    hx_top = np.mean(sorted(hx_values, reverse=True)[:max(1, round(len(hx_values) * 0.2))]).item()
     
     for i in range(len(ckpts)):
         if ckpt_idx == i:
