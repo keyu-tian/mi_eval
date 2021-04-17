7be9ad43de7883cc016b3def4e9f40a472b0d074
tky_hm_xps13h <k_code@163.com>
2021-04-17 19:10:45 +0800

    hx calc

--- mi/mi_calc.py
+++ mi/mi_calc.py
@@ -25,7 +25,7 @@ def calc_MI_features_inputs(verbose: bool, features: torch.Tensor, inputs: torch
     assert len(args) == num_targets
     
     P = min(cpu_count(), 4)
-    if P >= 2:
+    if P >= 4:
         with Pool(P) as pool:
             results = list(pool.imap(__calc_MI_h_x, args, chunksize=1))
     else:
