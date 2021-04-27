9bbecbe229168bae58c4483debfe67f029df14e1
tky_hm_xps13h <k_code@163.com>
2021-04-27 16:05:31 +0800

    save_fname_to_h

--- exp/liveness_valset/out_save_fname_to_h.sh
+++ exp/liveness_valset/out_save_fname_to_h.sh
@@ -10,4 +10,4 @@ PYTHONPATH=${PYTHONPATH}:${REL_PATH} GLOG_vmodule=MemcachedClient=-1 \
 spring.submit run --gpu -n${n_gpus} \
 --cpus-per-task=6 \
 --job-name MI_test \
-"python -u -m main --cfg_path=\"${cfg_file}\" --dataset=liveness --train --save_fname_to_h"
+"python -u -m main --cfg_path=\"${cfg_file}\" --dataset=liveness --save_fname_to_h"
